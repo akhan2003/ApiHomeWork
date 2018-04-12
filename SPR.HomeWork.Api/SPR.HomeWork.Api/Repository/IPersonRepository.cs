01a727dfd9d3c812844dddb0be4773def33effa6
akhan2003 <adnank2003@hotmail.com>
2018-04-12 12:01:25 -0500

    Added Person Repo

---
+++ SPR.HomeWork.Api/SPR.HomeWork.Api/Repository/IPersonRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SPR.HomeWork.Api.Models;
+
+namespace SPR.HomeWork.Api.Repository
+{
+    interface IPersonRepository
+    {
+        IEnumerable<Person> GetAll();
+        Person Add(Person person);
+    }
+}
---
+++ SPR.HomeWork.Api/SPR.HomeWork.Api/Repository/PersonRepository.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SPR.HomeWork.Api.Models;
+
+namespace SPR.HomeWork.Api.Repository
+{
+    public class PersonRepository: IPersonRepository
+    {
+        private List<Person> persons = new List<Person>();
+        private int _personId = 1;
+
+        public PersonRepository()
+        {
+            Add(new Person { FirstName = "Joe", LastName = "Schmoe", Gender = "Male", FavoriteColor = "Red", DateOfBirth = "10/1/1980" });
+            Add(new Person { FirstName = "Jane", LastName = "Schmoe", Gender = "Female", FavoriteColor = "Blue", DateOfBirth = "10/1/1982" });
+            Add(new Person { FirstName = "Steve", LastName = "Jobs", Gender = "Male", FavoriteColor = "Blue", DateOfBirth = "10/1/1960" });
+        }
+
+        public IEnumerable<Person> GetAll()
+        {
+            return persons;
+        }
+
+        public Person Add(Person person)
+        {
+            if (person == null)
+            {
+                throw new ArgumentNullException("person");
+            }
+
+            person.Id = _personId++;
+            persons.Add(person);
+            return person;
+        }
+
+    }
+}
